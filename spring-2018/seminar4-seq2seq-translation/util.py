01f9a5284b86b437267453bb8a19c628cef047b0
Emil Magerramov <lemhell@gmail.com>
2018-02-15 16:44:06 +0300

    Finished seminar-4-2 seq2seq practice, added pretrained models for german and spanish, added attention visualization

--- spring-2018/seminar4-seq2seq-translation/util.py
+++ spring-2018/seminar4-seq2seq-translation/util.py
@@ -194,3 +194,29 @@ def evaluateRandomly(encoder, decoder, n=10):
         output_sentence = ' '.join(output_words)
         print('<', output_sentence)
         print('')
+        
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+
+def evaluateAndShowAttention(input_sentence, encoder, decoder, input_lang, output_lang):
+    output_words, attentions = evaluate(
+        encoder, decoder, input_sentence, input_lang, output_lang)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    showAttention(input_sentence, output_words, attentions)
